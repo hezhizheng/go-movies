8d4cc150990a45a91dcda2b54d3a6f64cea1d056
hezhizheng <dexter.ho.cn@gmail.com>
2020-03-18 18:38:01 +0800

    index on master: a39a7f7 feat: search_m

--- controller/DebugController.go
+++ controller/DebugController.go
@@ -2,11 +2,10 @@ package controller
 
 import (
 	"github.com/julienschmidt/httprouter"
-	"go_movies/utils"
 	"net/http"
 )
 
 func Debug(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
 
-	go utils.DelAllListCacheKey()
+	// go utils.DelAllListCacheKey()
 }
