0e31b545ec27d0d1869c29d9be8ef7ede3404314
hzz <dexter.ho.cn@gmail.com>
2021-01-27 16:15:25 +0800

    misc: replace webpage = api.okzy.tv

---
+++ utils/Helper.go
@@ -0,0 +1,45 @@
+package utils
+
+import "strings"
+
+func InArray(needle interface{}, hystack interface{}) bool {
+	switch key := needle.(type) {
+	case string:
+		for _, item := range hystack.([]string) {
+			if key == item {
+				return true
+			}
+		}
+	case int:
+		for _, item := range hystack.([]int) {
+			if key == item {
+				return true
+			}
+		}
+	case int64:
+		for _, item := range hystack.([]int64) {
+			if key == item {
+				return true
+			}
+		}
+	default:
+		return false
+	}
+	return false
+}
+
+//
+// /index.php/vod/detail/id/1405.html => /?m=vod-detail-id-16250.html
+func Index2vod(Url string) string {
+	UrlStrSplit := strings.Split(Url, "/id/")[1]
+	id := strings.TrimRight(UrlStrSplit, ".html")
+
+	return "/?m=vod-detail-id-"+id+".html"
+}
+
+// /?m=vod-detail-id-16250.html => /index.php/vod/detail/id/1405.html
+func Vod2index(Url string) string {
+	UrlStrSplit := strings.Split(Url, "-id-")[1]
+	id := strings.TrimRight(UrlStrSplit, ".html")
+	return "/index.php/vod/detail/id/"+id+".html"
+}
